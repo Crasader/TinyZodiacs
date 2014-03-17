563914772a919f987346a5853dea4caef95aceb7
vominhnhut99 <vominhnhut9997>
2014-03-17 10:39:07 +0700

    Create move engine

---
+++ Classes/ActionEngine.h
@@ -0,0 +1,29 @@
+//
+//  ActionEngine.h
+//  TinyZodiacs
+//
+//  Created by Nhut on 3/17/14.
+//
+//
+
+#ifndef __TinyZodiacs__ActionEngine__
+#define __TinyZodiacs__ActionEngine__
+
+#include <iostream>
+#include "cocos2d.h"
+
+USING_NS_CC;
+class ActionEngine
+{
+private:
+public:
+    virtual void moveLeft()=0;
+    virtual void moveRight()=0;
+    virtual void jump()=0;
+    virtual void fallDown()=0;
+    virtual void attack()=0;
+    virtual void skill1()=0;
+    virtual void skill2()=0;
+protected:
+};
+#endif /* defined(__TinyZodiacs__ActionEngine__) */
---
+++ Classes/CharacterActionEngine.h
@@ -0,0 +1,35 @@
+//
+//  CharacterActionEngine.h
+//  TinyZodiacs
+//
+//  Created by Nhut on 3/17/14.
+//
+//
+
+#ifndef __TinyZodiacs__CharacterActionEngine__
+#define __TinyZodiacs__CharacterActionEngine__
+
+#include <iostream>
+#include "cocos2d.h"
+#include "ActionEngine.h"
+#include "Character.h"
+
+USING_NS_CC;
+class CharacterActionEngine: ActionEngine
+{
+private:
+protected:
+public:
+    CC_SYNTHESIZE(Character*, character, Character);
+    CharacterActionEngine(Character* character);
+    ~CharacterActionEngine();
+    
+    virtual void moveLeft();
+    virtual void moveRight();
+    virtual void jump();
+    virtual void fallDown();
+    virtual void attack();
+    virtual void skill1();
+    virtual void skill2();
+};
+#endif /* defined(__TinyZodiacs__CharacterActionEngine__) */
