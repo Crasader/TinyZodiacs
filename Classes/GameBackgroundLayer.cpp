4b3f92bfa772d09c7b05ac082ab1a0379c17bb4c
hoaphat <phatnguyen.it.khtn@gmail.com>
2014-03-19 08:31:53 +0700

    add new background

--- Classes/GameBackgroundLayer.cpp
+++ Classes/GameBackgroundLayer.cpp
@@ -30,35 +30,20 @@ bool GameBackgroundLayer::init()
    
     for(int i=0;i<5;i++)
     {
-        CCSprite* background1 = CCSprite::createWithSpriteFrameName("background1.png");
+        CCSprite* background1 = CCSprite::createWithSpriteFrameName("background2.jpg");
         background1->setAnchorPoint(ccp(0,0));
        
      
-        CCSprite* background2 = CCSprite::createWithSpriteFrameName("background2.png");
+        CCSprite* background2 = CCSprite::createWithSpriteFrameName("background1.png");
         background2->setAnchorPoint(ccp(0,0));
-        CCSprite* background2_2 = CCSprite::createWithSpriteFrameName("background2.png");
-        background2_2->setAnchorPoint(ccp(0,0));
-        background2_2->setScale(2);
-        CCSprite* background2_3 = CCSprite::createWithSpriteFrameName("background2.png");
-        background2_3->setAnchorPoint(ccp(0,0));
-        background2_3->setScale(3);
-        CCSprite* background2_4 = CCSprite::createWithSpriteFrameName("background2.png");
-        background2_4->setAnchorPoint(ccp(0,0));
-        background2_4->setScale(4);
-       
-
-        
-        CCSprite* background3 = CCSprite::createWithSpriteFrameName("background3.png");
-        background3->setAnchorPoint(ccp(0,0));
-        
+//
         
         parallaxBackgroundNode->addChild(background1, 0, ccp(0.1f,1), ccp(0+(background1->getContentSize().width-1)*i,80));
         parallaxBackgroundNode->addChild(background2, 4, ccp(0.9f,1.09f), ccp(0+(background2->getContentSize().width-1)*i,84));
-        parallaxBackgroundNode->addChild(background2_2, 3, ccp(0.6f,1.07f), ccp(0+(background2_2->getContentSize().width*background2_2->getScaleX()-1)*i,84));
-        parallaxBackgroundNode->addChild(background2_3, 2, ccp(0.3f,1.05f), ccp(0+(background2_3->getContentSize().width*background2_3->getScaleX()-1)*i,84));
-        parallaxBackgroundNode->addChild(background2_4, 1, ccp(0.2f,1.03f), ccp(0+(background2_4->getContentSize().width*background2_4->getScaleX()-1)*i,84));
+//        parallaxBackgroundNode->addChild(background3, 3, ccp(0.6f,1.07f), ccp(0+(background2_2->getContentSize().width*background3->getScaleX()-1)*i,84));
+       
 
-        parallaxBackgroundNode->addChild(background3, 5, ccp(1,1), ccp(0+(background3->getContentSize().width-1)*i,-120));
+
 //        
         parallaxBackgroundNode->setAnchorPoint(ccp(0,0));
         
